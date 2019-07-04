c27c92cc4856faf705ec208099daf3de2f647303
ynjacobs <ytj64611@gamil.com>
2019-07-04 11:22:21 -0400

    functions written based off of test suite

--- simon_says.js
+++ simon_says.js
@@ -1,1 +1,33 @@
+function echo(str){
+    var newStr = str;
+    return newStr
+}
+
+
+function shout(str) {
+    var res = str.toUpperCase();
+    return res
+}
+
+console.log( shout('hi'));
+
+function repeat(str, num) {
+    (str + ' ')
+    let strRep = (str + ' ').repeat(num).trim();
+    return strRep
+}
+ 
+function startOfWord(str, num) {
+    let strBeg = str.substring(0,num);
+    return strBeg
+}
+
+function firstWord(str) {
+    let strFirst = str.split(' ')[0];
+    return strFirst
+}
+
+
+
 module.exports = {echo: echo, shout: shout, repeat: repeat, startOfWord: startOfWord, firstWord: firstWord};
+
